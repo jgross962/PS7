e47e19a37783d98f9d09ca62f7f78914539a263d
JGross <Jonathan.Gross@wustl.edu>
2018-04-25 23:53:20 -0500

    Problem 2 done
    2 done.

--- Gross_PS7.R
+++ Gross_PS7.R
@@ -12,32 +12,41 @@ library(tidyr)
 # Import data as tbl
 setwd("C:/Users/jgros/documents/GitHub/PS7")
 crimes = tbl_df(read.csv("March2018.csv"))
-
+# Check data
 fix(crimes)
-## 2.)
 
+
+## Pre-2.) Clean Data
 ## I honestly have no idea how you could clean this data only using only dplyr functions. I am using tidyr package instead
-# Only want crimes which actually happened in march -- take out data from other months
-crimes = filter(crimes,substring(DateOccur,1,1)=="3")
-# Sort crimes into broader categories
-crimes = select(crimes, Description) %>%
-  mutate()
 
-#Compute Number of crimes per day? Which Crime Happened Most
-mtcars %>%
-  mutate(mpg=replace(mpg, cyl==4, NA)) %>%
-  as.data.frame()
+#Split DateOccured into Date and Time into two columns
+crimes = separate(crimes,DateOccur, into=c("date","time"),sep = " ")
+crimes$date = as.Date(crimes$date,format='%m/%d/%Y')
 
+# Only want crimes which actually happened in march 2018 -- take out data from other months
+crimes = crimes%>%
+  filter(date>=as.Date("2018-03-01")) %>%
+  filter(date<=as.Date("2018-03-31"))
 
-cr
-crimes %>%
-  mutate(Description = contains("Assault", ignore.case = TRUE), "Assault")
+# Sort crimes into broader categories (just get main crime)
+# Possibly still could be combined more, but much better than it was
+# Note: Get some warnings, but everything works fine
+crimes = separate(crimes,Description,into = c("crime","crimeDetail"),sep ="-")
+crimes = separate(crimes,crime,into = c("crime"),sep =" ")
+crimes = separate(crimes,crime,into = c("crime"),sep =",")
+crimes = separate(crimes,crime,into = c("crime"),sep ="/")
+## 2.) 
 
-
-# Add column for just day (delete time of day)
-#crimes = mutate(crimes, JustDate = select(crimes,num_range("3",1:5)) )
+#Compute Number of crimes per day. 
 by.day = crimes %>%
-  group_by(substring(DateOccur,1,9)) %>%
+  group_by(crime,date) %>%
   summarise(count=n()
   )
 by.day
+
+## Which Crime Happened Most?
+by.crime = crimes %>%
+  group_by(crime) %>%
+  summarise(count=n()
+  )
+by.crime
